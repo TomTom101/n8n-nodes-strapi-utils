053db89074aad463d08925a3445fbc785ffee5ae
TomTom101 <thobra@gmail.com>
2025-07-19 20:37:52 +0200

    init

--- nodes/StrapiUtils/StrapiUtils.node.ts
+++ nodes/StrapiUtils/StrapiUtils.node.ts
@@ -6,28 +6,34 @@ import type {
 } from 'n8n-workflow';
 import { NodeConnectionType, NodeOperationError } from 'n8n-workflow';
 
-export class ExampleNode implements INodeType {
+export class StrapiUtils implements INodeType {
 	description: INodeTypeDescription = {
-		displayName: 'Example Node',
-		name: 'exampleNode',
+		displayName: 'Strapi Utils Node',
+		name: 'StrapiUtils',
 		group: ['transform'],
 		version: 1,
-		description: 'Basic Example Node',
+		description: 'Strapi Utils Node',
 		defaults: {
-			name: 'Example Node',
+			name: 'Strapi Utils Node',
 		},
 		inputs: [NodeConnectionType.Main],
 		outputs: [NodeConnectionType.Main],
 		usableAsTool: true,
+		credentials: [
+			{
+				name: 'strapiApi',
+				required: false,
+			},
+		],
 		properties: [
 			// Node properties which the user gets displayed and
 			// can change on the node.
 			{
-				displayName: 'My String',
-				name: 'myString',
+				displayName: 'Strapi URL',
+				name: 'strapiUrl',
 				type: 'string',
 				default: '',
-				placeholder: 'Placeholder value',
+				placeholder: 'https://your-strapi-instance.com',
 				description: 'The description text',
 			},
 		],
